d9003c21d907474718db79c16eb0cfe024beb0d6
YuriDomingos <josemarlabareda@gmail.com>
2024-09-25 21:54:00 +0100

    Configurações Iniciais do Projecto

---
+++ src/app/icons-provider.ts
@@ -0,0 +1,19 @@
+import {
+    NZ_ICONS,
+    NzIconModule
+  } from 'ng-zorro-antd/icon';
+  
+  import {
+    MenuFoldOutline,
+    MenuUnfoldOutline,
+    FormOutline,
+    DashboardOutline
+  } from '@ant-design/icons-angular/icons';
+  
+  export const icons = [
+    MenuFoldOutline,
+    MenuUnfoldOutline,
+    FormOutline,
+    DashboardOutline
+  ];
+  
